02ed337bf1319efc563b167ffe2a217cbae54d15
deokbin <deokbin@naver.com>
2024-11-07 21:34:15 +0900

    feat(App) : 사용자 입력 및 결과 출력, 재고 확인 출력 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import productsData from './data/productsData.js';
+import parseCSV from './utils/parseCSV.js';
+import formatProductList from './utils/formatProductList.js';
+import { InputView } from './view/InputView.js';
+import { IOMessage } from './constants/IOMessage.js';
+import { OutputView } from './view/OutputView.js';
+import { Controller } from './controller/Controller.js';
+import PurchasedList from './model/PurchasedList.js';
+import Products from './model/Products.js';
+import promotionData from './data/promotionData.js';
+
 class App {
-  async run() {}
+  async run() {
+    OutputView.displayMessage(IOMessage.welcomeMessage);
+    OutputView.displayMessage(formatProductList(parseCSV(productsData)));
+    const userInput = await InputView.getUserInput(IOMessage.purchaseMessage);
+
+    const purchasedListModel = new PurchasedList();
+    const productModel = new Products();
+    const controller = new Controller(productModel, purchasedListModel);
+    controller.loadProductData(parseCSV(productsData));
+    controller.loadPromotionData(parseCSV(promotionData));
+    controller.handlePurchaseInput(userInput);
+    OutputView.displayMessage(purchasedListModel.getPurchasedItems());
+    OutputView.displayMessage(productModel.getStock());
+  }
 }
 
 export default App;
