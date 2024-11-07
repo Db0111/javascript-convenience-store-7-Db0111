eb553aca3e355fbab68c44a140f6ad833485c499
deokbin <deokbin@naver.com>
2024-11-07 11:03:22 +0900

    feat(IOMessage) : 필요한 안내 문구 상수 추가

---
+++ src/constants/IOMessage.js
@@ -0,0 +1,10 @@
+export const IOMessage = Object.freeze({
+  welcomeMessage: `안녕하세요. W편의점입니다. 현재 보유하고 있는 상품입니다.`,
+  purchaseMessage: `구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])`,
+  promotionApplyMessage: (product, count) =>
+    `현재 ${product}은(는) ${count}개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)`,
+  promotionLackMessage: (product, count) =>
+    `현재 ${product} ${count}개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)`,
+  membershipMessage: `멤버십 할인을 받으시겠습니까? (Y/N)`,
+  AddPurchaseMessage: `감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)`,
+});
