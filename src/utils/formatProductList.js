407e2edfd1113239d76fa31e0714f649d3e2a69d
deokbin <deokbin@naver.com>
2024-11-07 10:27:57 +0900

    fix(formatProductList) : 상품 목록 배열로 반환에서 string으로 반환

--- src/utils/formatProductList.js
+++ src/utils/formatProductList.js
@@ -1,17 +1,19 @@
 const formatProductList = productList => {
-  return productList.map(product => {
-    const { name, price, quantity, promotion } = product;
-    const priceFormatted = parseInt(price).toLocaleString();
-    let quantityFormatted = '재고 없음';
-    if (quantity > 0) {
-      quantityFormatted = `${quantity}개`;
-    }
-    let promotionFormatted = '';
-    if (promotion !== null && promotion !== 'null' && promotion) {
-      promotionFormatted = ` ${promotion}`;
-    }
-    return `- ${name} ${priceFormatted}원 ${quantityFormatted}${promotionFormatted}`;
-  });
+  return productList
+    .map(product => {
+      const { name, price, quantity, promotion } = product;
+      const priceFormatted = parseInt(price).toLocaleString();
+      let quantityFormatted = '재고 없음';
+      if (quantity > 0) {
+        quantityFormatted = `${quantity}개`;
+      }
+      let promotionFormatted = '';
+      if (promotion !== null && promotion !== 'null' && promotion) {
+        promotionFormatted = ` ${promotion}`;
+      }
+      return `- ${name} ${priceFormatted}원 ${quantityFormatted}${promotionFormatted}`;
+    })
+    .join('\n');
 };
 
 export default formatProductList;
