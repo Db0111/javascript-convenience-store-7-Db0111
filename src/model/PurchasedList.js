2f02024fef37d6978aa8ab55a47d5edbaf0ff5a4
deokbin <deokbin@naver.com>
2024-11-10 03:15:50 +0900

    feat(PurchasedList) : 사용자 구매 목록에 멤버심, 프로모션, 총액 적용 메서드 추가

--- src/model/PurchasedList.js
+++ src/model/PurchasedList.js
@@ -1,18 +1,45 @@
 class PurchasedList {
   constructor() {
-    this.items = {};
+    this.items = [];
+    this.promotionDiscountAmount = 0;
+    this.membershipDiscountAmount = 0;
   }
 
-  addProduct(name, quantity) {
+  addProduct(name, quantity, price) {
     if (this.items[name]) {
-      this.items[name] += quantity;
+      this.items[name].quantity += quantity;
     } else {
-      this.items[name] = quantity;
+      this.items[name] = {
+        quantity: quantity,
+        price: Number(price),
+      };
     }
   }
 
   getPurchasedItems() {
     return this.items;
+  }
+
+  applyMembershipDiscount(discountAmount) {
+    this.membershipDiscountAmount = discountAmount;
+  }
+
+  getTotalAmount() {
+    const total = Object.entries(this.items).reduce((sum, [name, item]) => {
+      return sum + item.price * item.quantity;
+    }, 0);
+    return total;
+  }
+
+  getPromotionDiscountAmount() {
+    return this.promotionDiscountAmount;
+  }
+
+  getFinalAmount() {
+    const finalAmount =
+      this.getTotalAmount() - this.promotionDiscountAmount - this.membershipDiscountAmount;
+
+    return finalAmount;
   }
 }
 export default PurchasedList;
