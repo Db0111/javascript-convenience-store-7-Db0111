be246fb7994f10e5918b0c6916334ed90d747d90
deokbin <deokbin@naver.com>
2024-11-10 06:08:30 +0900

    fix(Receipt): 가격 총액 오류 수정 - 총액 계산 - 사용자 입력값에 대해서만 하고 있었음 - 사용자 입력값 + 증정품에 대한 값까지 합산하여 계산

--- src/model/Receipt.js
+++ src/model/Receipt.js
@@ -8,7 +8,7 @@ class Receipt {
     this.membershipDiscount = 0;
   }
 
-  addItem(name, quantity) {
+  addItem(name, quantity, freeQuantity) {
     const productData = this.productModel.getStock()[name];
     let product;
 
@@ -21,9 +21,9 @@ class Receipt {
 
     this.items.push({
       name,
-      quantity: Number(quantity),
+      quantity: Number(quantity + freeQuantity),
       price,
-      subtotal: price * Number(quantity),
+      subtotal: price * Number(quantity + freeQuantity),
     });
   }
 
@@ -68,21 +68,21 @@ class Receipt {
 
   generateReceiptMessage() {
     let message = '==============W 편의점================\n';
-    message += '상품명\t\t수량\t\t금액\n';
+    message += '상품명\t\t수량\t금액\n';
 
     this.items.forEach(item => {
-      message += `${item.name}\t\t${item.quantity}\t\t${item.subtotal.toLocaleString()}\n`;
+      message += `${item.name}\t\t\t${item.quantity}\t${item.subtotal.toLocaleString()}\n`;
     });
 
     if (this.promotionItems.length > 0) {
       message += '=============증      정===============\n';
       this.promotionItems.forEach(item => {
-        message += `${item.name}\t\t${item.quantity}\n`;
+        message += `${item.name}\t\t\t${item.quantity}\n`;
       });
     }
 
     message += '======================================\n';
-    message += `총구매액\t${this.items.reduce((sum, item) => sum + item.quantity, 0)}\t\t${this.getTotalAmount().toLocaleString()}\n`;
+    message += `총구매액\t\t${this.items.reduce((sum, item) => sum + item.quantity, 0)}\t${this.getTotalAmount().toLocaleString()}\n`;
 
     if (this.promotionDiscount > 0) {
       message += `행사할인\t\t\t-${this.promotionDiscount.toLocaleString()}\n`;
