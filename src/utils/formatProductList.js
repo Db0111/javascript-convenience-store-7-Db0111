c166c34276fb39cc20697c8abe552831aa540ba8
deokbin <deokbin@naver.com>
2024-11-10 05:17:11 +0900

    refactor(formatProductList): 데이터 갱신을 위해 포매팅 함수 변경 -App.js에서 한번만 데이터 파일을 파싱해서 불러옴, 업데이트 될 떄 getStock() 의 형태에 맞게 수정

--- src/utils/formatProductList.js
+++ src/utils/formatProductList.js
@@ -1,19 +1,21 @@
-const formatProductList = productList => {
-  return productList
-    .map(product => {
-      const { name, price, quantity, promotion } = product;
-      const priceFormatted = parseInt(price).toLocaleString();
-      let quantityFormatted = '재고 없음';
-      if (quantity > 0) {
-        quantityFormatted = `${quantity}개`;
-      }
-      let promotionFormatted = '';
-      if (promotion !== null && promotion !== 'null' && promotion) {
-        promotionFormatted = ` ${promotion}`;
-      }
-      return `- ${name} ${priceFormatted}원 ${quantityFormatted}${promotionFormatted}`;
+function formatProductList(stockData) {
+  return Object.entries(stockData)
+    .map(([productName, productList]) => {
+      return productList
+        .map(product => {
+          const { price, quantity, promotion } = product;
+
+          let promotionDescription;
+          if (promotion.type !== 'null') {
+            promotionDescription = promotion.type;
+          } else {
+            promotionDescription = '';
+          }
+
+          return `- ${productName} ${price.toLocaleString()}원 ${quantity}개 ${promotionDescription}`;
+        })
+        .join('\n');
     })
     .join('\n');
-};
-
+}
 export default formatProductList;
