ef34eab2cd53cc59049f6ff853a882707c3f656d
deokbin <deokbin@naver.com>
2024-11-10 05:14:38 +0900

    refactor(IOMessage): 출력 메시지 계행 수정

--- src/constants/IOMessage.js
+++ src/constants/IOMessage.js
@@ -1,5 +1,5 @@
 export const IOMessage = Object.freeze({
-  welcomeMessage: `안녕하세요. W편의점입니다. 현재 보유하고 있는 상품입니다. \n`,
+  welcomeMessage: `안녕하세요. W편의점입니다.\n현재 보유하고 있는 상품입니다. \n`,
   purchaseMessage: `\n 구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1]) \n`,
   promotionApplyMessage: (product, count) =>
     `현재 ${product}은(는) ${count}개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)`,
