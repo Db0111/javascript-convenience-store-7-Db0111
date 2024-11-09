2d155552b3be740b1a67236ba5aba52ff8f1aed0
deokbin <deokbin@naver.com>
2024-11-10 05:19:55 +0900

    refactor(InputFormatTest) : 불필요 주석 제거

--- __tests__/InputFormatTest.js
+++ __tests__/InputFormatTest.js
@@ -3,8 +3,8 @@ import { splitArrays, mapArrtoObject } from '../src/utils/formatInput.js';
 describe('구매 내역을 입력받을 수 있다.', () => {
   test('구매한 물건을 쉼표 기준으로 분리한다.', () => {
     const input = '[콜라-2],[사이다-3]';
-    const result = splitArrays(input); // splitArrays 함수 호출
-    expect(result).toEqual(['콜라-2', '사이다-3']); // 배열 내용이 같은지 비교
+    const result = splitArrays(input); 
+    expect(result).toEqual(['콜라-2', '사이다-3']); 
   });
 
   test('정상적인 형태로 입력 받은 경우 대괄호 안에 있는 데이터를 파싱하여 객체로 나타낸다.', () => {
