b81326f9f431ac43c1fcadddeb93e0a3138c44d5
deokbin <deokbin@naver.com>
2024-11-10 03:17:05 +0900

    feat(App): 프로모션 및 멤버십 비즈니스 로직 추가 사용자 추가 구매 여부 N 답변 시 즉시 종료

--- src/App.js
+++ src/App.js
@@ -7,22 +7,58 @@ import { OutputView } from './view/OutputView.js';
 import { Controller } from './controller/Controller.js';
 import PurchasedList from './model/PurchasedList.js';
 import Products from './model/Products.js';
+import Promotion from './model/Promotion.js';
 import promotionData from './data/promotionData.js';
+import Membership from './model/Membership.js';
+import Receipt from './model/Receipt.js';
+import InputValidator from './utils/validator/InputValidator.js';
 
 class App {
   async run() {
-    OutputView.displayMessage(IOMessage.welcomeMessage);
-    OutputView.displayMessage(formatProductList(parseCSV(productsData)));
-    const userInput = await InputView.getUserInput(IOMessage.purchaseMessage);
-
     const purchasedListModel = new PurchasedList();
-    const productModel = new Products();
-    const controller = new Controller(productModel, purchasedListModel);
-    controller.loadProductData(parseCSV(productsData));
-    controller.loadPromotionData(parseCSV(promotionData));
-    controller.handlePurchaseInput(userInput);
-    OutputView.displayMessage(purchasedListModel.getPurchasedItems());
-    OutputView.displayMessage(productModel.getStock());
+    const promotionModel = new Promotion();
+    const productModel = new Products(promotionModel);
+    const membershipModel = new Membership();
+    let continueShopping = true;
+
+    const receipt = new Receipt(purchasedListModel, productModel);
+
+    const controller = new Controller(
+      productModel,
+      promotionModel,
+      purchasedListModel,
+      membershipModel,
+      receipt,
+    );
+    while (continueShopping) {
+      OutputView.displayMessage(IOMessage.welcomeMessage);
+      OutputView.displayMessage(formatProductList(parseCSV(productsData)));
+
+      const userInput = await InputView.getUserInput(IOMessage.purchaseMessage);
+
+      controller.loadProductData(parseCSV(productsData));
+      controller.loadPromotionData(parseCSV(promotionData));
+
+      await controller.handlePurchaseInput(userInput);
+
+      const membershipAnswer = await InputView.getUserInput(IOMessage.membershipMessage);
+      const validatedMembershipAnswer = InputValidator.validateYesNo(membershipAnswer);
+
+      if (validatedMembershipAnswer === 'Y') {
+        controller.applyMembershipDiscount();
+      }
+
+      await OutputView.displayMessage(receipt.generateReceiptMessage());
+
+      const continueAnswer = await InputView.getUserInput(IOMessage.AddPurchaseMessage);
+      const validatedAnswer = InputValidator.validateYesNo(continueAnswer);
+
+      if (validatedAnswer === 'N') {
+        continueShopping = false;
+
+        break;
+      }
+    }
   }
 }
 
