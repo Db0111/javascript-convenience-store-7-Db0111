2259f721384d3faa9ec827142eede4f757e47e43
deokbin <deokbin@naver.com>
2024-11-11 10:36:06 +0900

    refactor(MembershipService): 멤버십 비즈니스 로직 함수 길이 리팩토링

--- src/service/MembershipService.js
+++ src/service/MembershipService.js
@@ -1,15 +1,9 @@
 export function handleMembership(membershipAnswer, purchasedListModel, membershipModel, receipt) {
-  console.log('Validated Membership Answer:', membershipAnswer);
   if (membershipAnswer === 'Y') {
-    const totalAmount = purchasedListModel.getTotalAmount();
-    const promotionDiscountAmount = purchasedListModel.getPromotionDiscountAmount();
-    console.log(`총 금액: ${totalAmount}, 프로모션 할인 금액: ${promotionDiscountAmount}`);
-
     const membershipDiscount = membershipModel.calculateDiscount(
-      totalAmount,
-      promotionDiscountAmount,
+      purchasedListModel.getTotalAmount(),
+      purchasedListModel.getPromotionDiscountAmount(),
     );
-    console.log('Membership Discount:', membershipDiscount);
     purchasedListModel.applyMembershipDiscount(membershipDiscount);
     receipt.setMembershipDiscount(membershipDiscount);
   }
