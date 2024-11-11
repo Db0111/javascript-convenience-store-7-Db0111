7192b181f60265627b6ce8d6b2c83a3f5a2dd694
deokbin <deokbin@naver.com>
2024-11-11 09:49:17 +0900

    refactor(handleMembership): 불필요 콘솔 출력 코드 제거

--- src/service/MembershipService.js
+++ src/service/MembershipService.js
@@ -1,11 +1,15 @@
 export function handleMembership(membershipAnswer, purchasedListModel, membershipModel, receipt) {
+  console.log('Validated Membership Answer:', membershipAnswer);
   if (membershipAnswer === 'Y') {
     const totalAmount = purchasedListModel.getTotalAmount();
     const promotionDiscountAmount = purchasedListModel.getPromotionDiscountAmount();
+    console.log(`총 금액: ${totalAmount}, 프로모션 할인 금액: ${promotionDiscountAmount}`);
+
     const membershipDiscount = membershipModel.calculateDiscount(
       totalAmount,
       promotionDiscountAmount,
     );
+    console.log('Membership Discount:', membershipDiscount);
     purchasedListModel.applyMembershipDiscount(membershipDiscount);
     receipt.setMembershipDiscount(membershipDiscount);
   }
