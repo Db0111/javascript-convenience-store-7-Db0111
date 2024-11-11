ac5c3b4481a64575c96d1beb1138e1290be7216f
deokbin <deokbin@naver.com>
2024-11-11 11:52:58 +0900

    refactor(App): 함수 메서드 분리

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { InputView } from './view/InputView.js';
 import { IOMessage } from './constants/IOMessage.js';
 import { OutputView } from './view/OutputView.js';
 import { Controller } from './controller/Controller.js';
+import { handleStockCheck } from './service/StockCheckService.js';
 import PurchasedList from './model/PurchasedList.js';
 import Products from './model/Products.js';
 import Promotion from './model/Promotion.js';
@@ -15,14 +16,30 @@ import InputValidator from './utils/validator/InputValidator.js';
 
 class App {
   async run() {
+    const [purchasedListModel, promotionModel, productModel, membershipModel, receipt, controller] =
+      this.initModels();
+    this.purchasedListModel = purchasedListModel;
+    this.receipt = receipt;
+
+    await this.loadData(controller);
+
+    let continueShopping = true;
+
+    while (continueShopping) {
+      await this.displayWelcomeMessage(productModel);
+      let userInput = await this.handleUserInput(productModel);
+      if (!userInput) continue;
+      await this.processPurchase(userInput, controller, receipt);
+      continueShopping = await this.askForContinue();
+    }
+  }
+
+  initModels() {
     const purchasedListModel = new PurchasedList();
     const promotionModel = new Promotion();
     const productModel = new Products(promotionModel);
     const membershipModel = new Membership();
-    let continueShopping = true;
-
     const receipt = new Receipt(purchasedListModel, productModel);
-
     const controller = new Controller(
       productModel,
       promotionModel,
@@ -30,27 +47,55 @@ class App {
       membershipModel,
       receipt,
     );
+    return [purchasedListModel, promotionModel, productModel, membershipModel, receipt, controller];
+  }
+
+  async displayWelcomeMessage(productModel) {
+    OutputView.displayMessage(IOMessage.welcomeMessage);
+    OutputView.displayMessage(formatProductList(productModel.getStock()));
+  }
+
+  async loadData(controller) {
     controller.loadProductData(parseCSV(productsData));
     controller.loadPromotionData(parseCSV(promotionData));
+  }
 
-    while (continueShopping) {
-      OutputView.displayMessage(IOMessage.welcomeMessage);
-      OutputView.displayMessage(formatProductList(productModel.getStock()));
-      const userInput = await InputView.getUserInput(IOMessage.purchaseMessage);
-
-      await controller.handlePurchaseInput(userInput);
-
-      await OutputView.displayMessage(receipt.generateReceiptMessage());
-
-      const continueAnswer = await InputView.getUserInput(IOMessage.AddPurchaseMessage);
-      const validatedAnswer = InputValidator.validateYesNo(continueAnswer);
-
-      if (validatedAnswer === 'N') {
-        continueShopping = false;
-
-        break;
+  async handleUserInput(productModel) {
+    let isStockAvailable = false;
+    let userInput;
+    while (!isStockAvailable) {
+      try {
+        userInput = await InputView.getUserInput(IOMessage.purchaseMessage);
+        if (!userInput) {
+          continue;
+        }
+        userInput = await handleStockCheck(userInput, productModel);
+        isStockAvailable = true;
+      } catch (error) {
+        OutputView.displayError(error.message);
+        continue;
       }
     }
+    return userInput;
+  }
+
+  async processPurchase(userInput, controller, receipt) {
+    await controller.handlePurchaseInput(userInput);
+    await OutputView.displayMessage(receipt.generateReceiptMessage());
+  }
+
+  async askForContinue() {
+    const continueAnswer = await InputView.getUserInput(IOMessage.AddPurchaseMessage);
+    const validatedAnswer = InputValidator.validateYesNo(continueAnswer);
+    if (validatedAnswer === 'Y') {
+      this.clearPreviousData();
+    }
+    return validatedAnswer === 'Y';
+  }
+
+  clearPreviousData() {
+    this.purchasedListModel.clear();
+    this.receipt.clear();
   }
 }
 
