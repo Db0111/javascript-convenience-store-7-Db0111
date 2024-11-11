239cf87083a6fe00058809720df26ad2bff727b7
deokbin <deokbin@naver.com>
2024-11-11 14:08:52 +0900

    feat(MagicNumber): 매직 넘버에 멤버십 관련 상수 추가

--- src/constants/MagicNumber.js
+++ src/constants/MagicNumber.js
@@ -1,1 +1,7 @@
 export const Regex = /^\[\s*[^-\s]+\s*-\s*\d+\s*\](,\s*\[\s*[^-\s]+\s*-\s*\d+\s*\])*$/;
+
+export const MEMBERSHIP = {
+  DISCOUNT_RATE: 0.3,
+  MAX_DISCOUNT: 8000,
+  ROUNDING_UNIT: 1000,
+};
